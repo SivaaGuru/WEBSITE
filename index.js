128a6fa2d0b2d5d94a3e08b7f9ecc746aecd5f8a
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-24 17:31:16 +0530

    hello

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-//console.log(`Hello`);
-//console.log(`I like pizza`);
+// let username;
+// document.getElementById("mySubmit").onclick = function(){
+//    username = document.getElementById("myText").value;
+//    document.getElmentBybyId("myH1") .textcontent = `Hello ${username}`
+// }
+ 
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
 
-//window.alert(`this is an alert!`)
-//window.alert(`I like pizza!`)
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+// const PI = 3.14159
+// let radius;
+// let circumference;
+
+// document.getElementById("mysubmit").onclick = function(){
+// radius =  document.getElementById("myText").value;
+// radius = Number(radius);
+// circumference = 2 * PI * radius;
+// document.getElementById("myH3").textcontent = circumference + "cm";
+
+
+
+// }
+
+
+
+
+
+
+
