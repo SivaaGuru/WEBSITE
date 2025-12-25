648496018ad3d376a5c03dedd5202f09fc674618
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-25 12:48:37 +0530

    my message

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-// let username;
-// document.getElementById("mySubmit").onclick = function(){
-//    username = document.getElementById("myText").value;
-//    document.getElmentBybyId("myH1") .textcontent = `Hello ${username}`
-// }
- 
-// let x = "pizza";
-// let y = "pizza";
-// let z = "pizza";
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increasetBtn = document.getElementById("increaseBtn");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
 
-// x = Number(x);
-// y = String(y);
-// z = Boolean(z);
-
-// console.log(x, typeof x);
-// console.log(y, typeof y);
-// console.log(z, typeof z);
-
-
-// const PI = 3.14159
-// let radius;
-// let circumference;
-
-// document.getElementById("mysubmit").onclick = function(){
-// radius =  document.getElementById("myText").value;
-// radius = Number(radius);
-// circumference = 2 * PI * radius;
-// document.getElementById("myH3").textcontent = circumference + "cm";
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
 
 
 
-// }
+
+
+
 
 
 
