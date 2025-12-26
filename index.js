2ae1442a89e5d937b7013802b1007a452c1ee505
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-26 10:31:20 +0530

    if statement

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const myButton = document.getElementById("myButton");
-const Label1 = document.getElementById("Label1");
-const Label2 = document.getElementById("Label2");
-const Label3 = document.getElementById("Label3");
-const min = 1;
-const max = 6;
-let randomNum1;
-let randomNum2;
-let randomNum3;
+// const myButton = document.getElementById("myButton");
+// const Label1 = document.getElementById("Label1");
+// const Label2 = document.getElementById("Label2");
+// const Label3 = document.getElementById("Label3");
+// const min = 1;
+// const max = 6;
+// let randomNum1;
+// let randomNum2;
+// let randomNum3;
 
 
-myButton.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    myLabel1.textContent = randomNum1;
-    myLabel2.textContent = randomNum2;
-    myLabel3.textContent = randomNum3;
+// myButton.onclick = function(){
+//     randomNum1 = Math.floor(Math.random() * max) + min;
+//     randomNum2 = Math.floor(Math.random() * max) + min;
+//     randomNum3 = Math.floor(Math.random() * max) + min;
+//     myLabel1.textContent = randomNum1;
+//     myLabel2.textContent = randomNum2;
+//     myLabel3.textContent = randomNum3;
 
-}
+// }
+
+
+
+
+// let age = 18;
+// let hasLicense = true;
+
+// if(age >= 16){
+//     console.log("You are old enough to drive");
+
+//     if(hasLicense){
+//         console.log("You have your license")
+//     }
+//     else{
+//         console.log("You do not have your license yet")
+//     }
+// }
+// else{
+//     console.log("You must be 16+ to have a license");
+// }
 
 
 
