b1743e5ea02065ba4a061f5db3fc6a5ed3a966ea
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-29 17:44:58 +0530

    message

--- index.js
+++ index.js
@@ -197,22 +197,70 @@
 
 
 
-let loggedIn = false;
-let username;
-let password;
+// let loggedIn = false;
+// let username;
+// let password;
 
-while(!loggedIn){
-    username = window.prompt("Enter your username");
-    password = window.prompt("Enter your password")
+// while(!loggedIn){
+//     username = window.prompt("Enter your username");
+//     password = window.prompt("Enter your password")
     
-    if (username === "myusername" && password === "mypassword"){
-        loggedIn = true;
-        console.log("You are logged in!");
-    }
-    else{
+//     if (username === "myusername" && password === "mypassword"){
+//         loggedIn = true;
+//         console.log("You are logged in!");
+//     }
+//     else{
 
+//     }
+//  }
+
+
+const minNum = 50;
+const maxNum = 100;
+const answer =Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+console.log(answer);
+
+let attempts = 0;
+let guess;
+let running = true; 
+
+while(running){
+
+    guess = window.prompt('Guess a number between ${miNum} - ${maxNum}');
+    guess = Number(guess);
+   
+
+    if(isNan(guess)){
+        window.alert("Please enter a valid number");
     }
- }
+    else if(guess < minNum ||guess > maxNum){
+        window.alert("Please enetr a valid number"); 
+    }else{
+        attempts++;
+        if(guess < answer){
+            window.alert("TOO LOW! TRY AGAIN!");
+        }
+        else if(guess > answer){
+            window.alert("TOO HIGH! TRY AGAIN!");
+        }
+        else{
+
+        }
+    }
+    running = false;
+
+}
+    
+    
+
+
+
+    running = false;      
+}
+
+
+
+
 
 
 
