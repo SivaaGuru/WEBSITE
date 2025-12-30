bd85e3edb0b4934c80d6432e46fc904714bedfea
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-30 10:47:43 +0530

    my message

--- index.js
+++ index.js
@@ -215,50 +215,60 @@
 //  }
 
 
-const minNum = 50;
-const maxNum = 100;
-const answer =Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-console.log(answer);
+// const minNum = 50;
+// const maxNum = 100;
+// const answer =Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+// console.log(answer);
 
-let attempts = 0;
-let guess;
-let running = true; 
+// let attempts = 0;
+// let guess;
+// let running = true; 
 
-while(running){
+// while(running){
 
-    guess = window.prompt('Guess a number between ${miNum} - ${maxNum}');
-    guess = Number(guess);
+//     guess = window.prompt('Guess a number between ${miNum} - ${maxNum}');
+//     guess = Number(guess);
    
 
-    if(isNan(guess)){
-        window.alert("Please enter a valid number");
-    }
-    else if(guess < minNum ||guess > maxNum){
-        window.alert("Please enetr a valid number"); 
-    }else{
-        attempts++;
-        if(guess < answer){
-            window.alert("TOO LOW! TRY AGAIN!");
-        }
-        else if(guess > answer){
-            window.alert("TOO HIGH! TRY AGAIN!");
-        }
-        else{
+//     if(isNan(guess)){
+//         window.alert("Please enter a valid number");
+//     }
+//     else if(guess < minNum ||guess > maxNum){
+//         window.alert("Please enetr a valid number"); 
+//     }else{
+//         attempts++;
+//         if(guess < answer){
+//             window.alert("TOO LOW! TRY AGAIN!");
+//         }
+//         else if(guess > answer){
+//             window.alert("TOO HIGH! TRY AGAIN!");
+//         }
+//         else{
 
-        }
-    }
-    running = false;
+//         }
+//     }
+//     running = false;
 
-}
+// }
     
     
 
 
 
-    running = false;      
+//     running = false;      
+// }
+
+
+
+
+
+function isValidEmail(email){
+
+    return email.includes("@") ? true:false
 }
 
-
+console.log(isValidEmail("Bro@fake.com"));
+console.log(isValidEmail("Elonmush.com"));
 
 
 
