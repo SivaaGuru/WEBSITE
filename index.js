4e6fb0282e14fd6c5f192874634dcd0e1e616785
SivaaGuru <sivaaguruuuuuu@gmail.com>
2025-12-30 11:16:23 +0530

    my message

--- index.js
+++ index.js
@@ -262,13 +262,37 @@
 
 
 
-function isValidEmail(email){
 
-    return email.includes("@") ? true:false
+// function isValidEmail(email){
+
+//     return email.includes("@") ? true:false
+// }
+
+// console.log(isValidEmail("Bro@fake.com"));
+// console.log(isValidEmail("Elonmush.com"));
+
+
+
+
+
+
+
+
+let x = 3;
+
+function2();
+
+function function1(){
+    let x = 1;
+    console.log(x);
 }
 
-console.log(isValidEmail("Bro@fake.com"));
-console.log(isValidEmail("Elonmush.com"));
+function function2(){
+    let x = 2;
+    console.log(x);
+}
+ 
+
 
 
 
