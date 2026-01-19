ada27100f1555df60814de555110bfb77e103031
SivaaGuru <sivaaguruuuuuu@gmail.com>
2026-01-19 17:18:47 +0530

    message

--- index.js
+++ index.js
@@ -867,14 +867,185 @@
 
 
 //sort method
-const people = [{name:"Spongebob",age: 30,gpa: 3.0},
-                {name:"Patrick",age: 37,gpa: 1.5},
-                {name:"Squidward",age: 51,gpa: 2.5},
-                {name:"sandy",age:27,gpa:4.00}]
+// const people = [{name:"Spongebob",age: 30,gpa: 3.0},
+//                 {name:"Patrick",age: 37,gpa: 1.5},
+//                 {name:"Squidward",age: 51,gpa: 2.5},
+//                 {name:"sandy",age:27,gpa:4.00}]
 
-people.sort((a,b) => a.age - b.age);
+// people.sort((a,b) => a.age - b.age);
 
-console.log(people);
+// console.log(people);
+
+
+
+//Fisher yates algorithm
+
+// const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+// shuffle(cards);
+
+// console.log(cards);
+
+// function shuffle(array){
+//     for(let i = array.length - 1; i > 0; i--){
+//         const random = Math.floor(Math.random() * (i + 1));
+
+//         [array[i],array[random]] = [array[random], array[i]];
+//     }
+// }
+
+
+//closures
+
+// function increment (){
+//     let count = 0;
+//     count++;
+//     console.log(`Count increased to ${count}`);     
+// }
+
+// increment();
+
+// function createGame(){
+//     let score = 0;
+
+//     function increaseScore(points){
+//         score += points;
+//         console.log(`+${points}pts`);
+//     }
+
+//     function decreaseScore(points){
+//         score -=  points;
+//         console.log(`-${points}pts`);
+//     }
+
+//     function getScore(){
+//         return score;
+//     }
+//     return {increaseScore, decreaseScore, getScore};
+// }
+
+//set timeout
+
+
+// let timeoutId;
+
+// function startTimer(){
+//    timeoutId = setTimeout (() => window.alert("Hello"), 3000);
+//    console.log("STARTED");
+// }
+
+// function clearTimer(){
+//     clearTimeout(timeoutId);
+//     console.log("CLEARED");
+// }
+
+//digital clock program
+
+// function updateClock(){
+
+//     const now = new Date();
+//     let hours =now.getHours();
+//     const meridiem = hours >= 12 ? "PM" : "AM";
+//     hours = hours % 12 || 12;
+//     hours = hours.toString().padStart(2,0);
+//     const minutes = now.getMinutes().toString().padStart(2, 0);
+//     const seconds = now.getSeconds().toString().padStart(2, 0);
+//     const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`; 
+//     document.getElementById("clock").textContent = timeString;
+
+// }
+// updateClock();
+// setInterval(updateClock, 1000);
+
+
+
+//stopwatch program
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){  
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+function reset(){
+    clearInterval(timer);
+     startTime = 0;
+     elapsedTime = 0;
+     isRunning = false;
+     display.textContent = "00:00:00:00";
+}
+
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 /10);
+
+    hours =String(hours).padStart(2, "0");
+    minutes =String(minutes).padStart(2, "0");
+    seconds =String(seconds).padStart(2, "0");
+    milliseconds =String(milliseconds).padStart(2, "0");         
+    
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
