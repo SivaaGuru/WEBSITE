61ca7b9b193c967b005a2951e624d63373298ec9
SivaaGuru <sivaaguruuuuuu@gmail.com>
2026-03-03 17:35:57 +0530

    my messaage

---
+++ weatherapp/weatherapp.js
@@ -0,0 +1,34 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "01b66df9e5b7a93b3e5e6a3da36f9c1d";
+
+weatherForm.addEventListener("submit", event => {
+
+});
+
+async function getWeatherData(city){
+    
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+}
+
+
+
+
+
+
+
+
+
+
