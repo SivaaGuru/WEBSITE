be9a9f2bdd45e1165034a2cb724fc8df71b86554
SivaaGuru <sivaaguruuuuuu@gmail.com>
2026-03-09 17:11:19 +0530

    my messaage

--- shoppingcart/shoppingcart.js
+++ shoppingcart/shoppingcart.js
@@ -1,23 +1,83 @@
 let shop = document.getElementById("shop");
 
-console.log(shop);
+let shopItemsData = [{
+    id:"fsiufhs",
+    name:"Casual shirt",
+    price:45,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-1.jpg",},{id:"vbvbxmv",
+    name:"Office shirt",
+    price:100,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-2.jpg"},{id:"djkgajdhakj",
+    name:"T shirt",
+    price:25,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-3.jpg"},{id:"wwwwwwe",
+    name:"Mens suit",
+    price:300,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-4.jpg"}];
 
-let generateShop =() => {
-    return  shop.innerHTML = ` <div class="item">
-            <img width="220" src="images/img-1.jpg" alt="">
+let basket =[]
+
+
+let generateShop = () => {
+    return  (shop.innerHTML = shopItemsData
+        .map((x)=>{
+            let {id,name,price,desc,img} =x
+        return `<div id=product-id-${id} class="item">
+            <img width="220" src=${img} alt="">
             <div class="details">
-                <h3>Casual Shirt</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <h3>${name}</h3>
+                <p>${desc}</p>
                 <div class="price-qunatity"></div>
-                <h2>$ 45</h2>
+                <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                   <i class="bi bi-plus-lg"></i> 
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">0</div>
+                   <i onclick="increment(${id})" class="bi bi-plus-lg"></i> 
+                   </div>
                 </div>
             </div>
-        </div>
-     `;
+        </div>`;
+        
+    }).join(""));
+};
+
+
+generateShop();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id ===selectedItem.id); 
+    
+    if(search === undefined){
+    basket.push({
+        id: selectedItem.id,
+        item: 1,
+    });
+    }
+    else{
+        search.item += 1;
+    }
+    // console.log(basket);
+     update(selectedItem.id);
+};
+let decrement = (id) => {
+   let selectedItem = id;
+    let search = basket.find((x)=> x.id ===selectedItem.id); 
+    
+    if(search.item === 0) return;
+        else{
+        search.item -= 1;
+    }
+     //console.log(basket);
+     update();
+};
+let update = (id) => {
+    let search = basket.find((x)=>x.id === id);
+    console.log(search.item);
 };
 
 
