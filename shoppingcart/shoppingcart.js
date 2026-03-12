735c3e1daeb82268e6fd4aab13c12461561a20d8
SivaaGuru <sivaaguruuuuuu@gmail.com>
2026-03-12 12:12:37 +0530

    my messaage

--- shoppingcart/shoppingcart.js
+++ shoppingcart/shoppingcart.js
@@ -19,13 +19,14 @@ let shopItemsData = [{
     desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
     img:"images/img-4.jpg"}];
 
-let basket =[]
+let basket =JSON.parse(localStorage.getItem("data")) || [];
 
 
 let generateShop = () => {
     return  (shop.innerHTML = shopItemsData
         .map((x)=>{
             let {id,name,price,desc,img} =x
+            let search = basket.find((x)=>x.id === id) || []
         return `<div id=product-id-${id} class="item">
             <img width="220" src=${img} alt="">
             <div class="details">
@@ -35,7 +36,9 @@ let generateShop = () => {
                 <h2>$ ${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">
+                    ${search.item === undefined ? 0:search.item}
+                    </div>
                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i> 
                    </div>
                 </div>
@@ -61,6 +64,7 @@ let increment = (id) => {
     else{
         search.item += 1;
     }
+    localStorage.setItem("data", JSON.stringify(basket));
     // console.log(basket);
      update(selectedItem.id);
 };
@@ -72,10 +76,19 @@ let decrement = (id) => {
         else{
         search.item -= 1;
     }
+    localStorage.setItem("data", JSON.stringify(basket));
      //console.log(basket);
-     update();
+     update(selectedItem.id);
 };
 let update = (id) => {
     let search = basket.find((x)=>x.id === id);
-    console.log(search.item);
+    //console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
 };
+
+let calculation =()=>{
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y)=> x + y, 0)  
+};
+calculation();
